4dca52326f39f5478b6d84b636fce481caf34a8a
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2024-11-14 09:02:40 +0700

    pembaruan code

--- main.java
+++ main.java
@@ -4,6 +4,7 @@ public class main {
 
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
+    
         Dosen dosen2 = new Dosen("19700105", "Muhammad, S.T, M.T", "197001");
         TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
         TenagaKependidikan tendik2 = new TenagaKependidikan("19650304", "Rika, S.T.", "Tenaga Laboratorium");
